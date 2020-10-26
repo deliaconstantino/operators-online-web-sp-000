ced21b0e26a5fefb83c0f2489d09d7c65e8a00e5
Delia Constantino <dconstan92@gmail.com>
2020-10-26 19:38:11 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,13 @@
 require 'pry'
 
 def unsafe?(speed)
- if speed > 60
-   binding.pry
+ if speed > 60 && speed < 40
    return true
-
-
  end
 end
 
 
 
-# def not_safe?(speed)
-# speed > 40 && speed < 60 ? false : true
-# end
+def not_safe?(speed)
+speed > 40 && speed < 60 ? false : true
+end
